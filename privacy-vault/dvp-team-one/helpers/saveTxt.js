1db67dd8e174e29ca97315df7dca1048bcb337fc
Ander-P <pradoanderson1203@outlook.com>
2024-04-23 12:28:53 -0500

    feat: update function of savetxt

--- privacy-vault/dvp-team-one/helpers/saveTxt.js
+++ privacy-vault/dvp-team-one/helpers/saveTxt.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 
-
 function saveTxt(datos) {
     const contenido = datos.map(obj => `key: "${obj.key}", value: "${obj.value}"`).join('\n');
+    const nombreArchivo = "base_de_datos.txt";    
     try {
-      fs.writeFileSync("base_de_datos.txt", contenido);
-      console.log('El archivo fue guardado correctamente.');
+      fs.writeFileSync(nombreArchivo, contenido);
     } catch (error) {
       console.error('Hubo un error al guardar el archivo:', error);
     }
-}
+  }
 
 module.exports = { saveTxt };
   
