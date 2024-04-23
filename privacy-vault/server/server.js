365154cef1fda637fa169fcd7cd050fe616d286b
Ricardo Aguilera Zamarripa <16416217+jeleputs@users.noreply.github.com>
2024-04-23 11:30:48 -0600

    Updating files and folder structure so both the front end and back end apps run

--- privacy-vault/server/server.js
+++ privacy-vault/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3001;
+const port = 3006;
 
 // Middleware para analizar el cuerpo de las solicitudes como JSON
 app.use(bodyParser.json());
